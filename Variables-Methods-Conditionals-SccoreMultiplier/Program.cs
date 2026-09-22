d842304a7ca540c1443908650042df3684ead4c8
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-22 17:25:51 -0300

    Fixed my confusion
    It now has multiplier existing always in the score and a method has been added to increase the multiplier itself.

--- Variables-Methods-Conditionals-SccoreMultiplier/Program.cs
+++ Variables-Methods-Conditionals-SccoreMultiplier/Program.cs
@@ -5,39 +5,62 @@ namespace Variables_Methods_Conditionals_SccoreMultiplier
     internal class Program
     {
         static int currentScore;
-        static int currentScoreMultiplier;
+        static int currScoreMulti;
 
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.White;
             currentScore = 0; 
-            currentScoreMultiplier = 1;
+            currScoreMulti = 1;
 
+            Console.WriteLine("Score Multiplier: " + currScoreMulti);
             HUD();
-            AddScore(5);
-
             AddScoreWithMultiplier(5);
             HUD();
+            IncreaseScoreMultiplier();
+            HUD();
+            AddScoreWithMultiplier(10);
 
-            //change to be two methods one has multiplier in it all the time
-            //and one only changes the multpilier itself
+            HUD();
+            AddScoreWithMultiplier(20);
+            HUD();
+            IncreaseScoreMultiplier();
+            HUD();
+            AddScoreWithMultiplier(30);
+
+            HUD();
+            AddScoreWithMultiplier(15);
+            HUD();
+            IncreaseScoreMultiplier();
+            HUD();
+            AddScoreWithMultiplier(5);
+            HUD();
         }
 
-        static void AddScore(int incomingPoints)
+        static void AddScoreWithMultiplier(int incomingPoints)
         {
-            currentScore = currentScore + incomingPoints;
-            Console.WriteLine("\nYou scored " + incomingPoints + " points.");
+            //change to be two methods one has multiplier in it all the time
+            Console.WriteLine("\nThe Math: " + currentScore + " +(" + incomingPoints + "x" + currScoreMulti + ")");
+            currentScore = currentScore + incomingPoints * currScoreMulti;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("You Scored " + incomingPoints + "pts.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        static void IncreaseScoreMultiplier()
+        {
+            //and one only changes the multpilier itself
+            Console.ForegroundColor = ConsoleColor.Green;
+            currScoreMulti++;
+            Console.WriteLine("\nIncrease Multiplier: "+currScoreMulti);
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         static void HUD()
         {
             Console.WriteLine("\n---The HUD!---");
             Console.WriteLine("Score: " + currentScore);
-        }
-
-        static void AddScoreWithMultiplier(int incomingPoints)
-        {
-            currentScore = currentScore + incomingPoints * currentScoreMultiplier;
-            Console.WriteLine("\nYou scored " + (incomingPoints * currentScoreMultiplier) + " (" + incomingPoints + "*" + currentScoreMultiplier + ") points.");
         }
     }
 }
